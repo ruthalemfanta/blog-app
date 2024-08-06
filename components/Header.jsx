8e42ac670d61a2083d314579f48b4b524dc26056
ruthalemfanta <133218569+ruthalemfanta@users.noreply.github.com>
2024-08-06 22:25:07 +0300

    Header update

--- components/Header.jsx
+++ components/Header.jsx
@@ -12,6 +12,12 @@ const Header = () => {
         </div>
         <div className='text-center my-8'>
             <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
+            <p className="mt-10 max-w-[740px] m-auto text-xs sm:text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi deserunt totam accusamus quaerat et veritatis. Commodi nisi iste doloribus provident voluptatum iusto, illo tempore corporis voluptates dolore, asperiores voluptatibus? Unde.
+            </p>
+            <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0_#000000]' action="">
+              <input type="email" placeholder='Enter your email' className='pl-4 outline-none' />
+              <button className='border-1 border-black py-4 px-4 sm:px-8 hover:bg-gray-600 active:bg-gray-600 active:text-white'>Subscribe</button>
+            </form>
         </div>
     </div>
   )
